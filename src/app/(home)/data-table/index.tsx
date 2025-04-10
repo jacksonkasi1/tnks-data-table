d66640afcd2d91cfc5a813a88068fb3f37d28bfe
jacksonkasi1 <jacksonkasipeacock@gmail.com>
2025-04-10 22:05:24 +0530

    Add AddButton component to UserTable for user addition functionality
    Introduce a new AddButton component in the UserTable, enhancing the DataTable with a customizable toolbar element. This update improves user interaction by allowing the addition of new users directly from the table interface.

--- src/app/(home)/data-table/index.tsx
+++ src/app/(home)/data-table/index.tsx
@@ -1,5 +1,11 @@
 "use client";
 
+// ** Import 3rd Party Libs
+import { PlusIcon } from "lucide-react";
+
+// ** Import UI Components
+import { Button } from "@/components/ui/button";
+
 // ** Import Date Table
 import { DataTable } from "@/components/data-table/data-table";
 
@@ -11,6 +17,13 @@ import { useExportConfig } from "./utils/config";
 import { fetchUsersByIds } from "@/api/user/fetch-users-by-ids";
 import { fetchUsers } from "@/api/user/fetch-users";
 
+const AddButton = () => (
+  <Button size="sm" variant="outline">
+    <PlusIcon className="mr-2 h-4 w-4" />
+    Add User
+  </Button>
+);
+
 export default function UserTable() {
   return (
     <DataTable
@@ -20,6 +33,7 @@ export default function UserTable() {
       fetchByIdsFn={fetchUsersByIds}
       idField="id"
       pageSizeOptions={[10, 20, 30, 40, 50, 100, 150]}
+      customToolbarComponent={<AddButton />}
       config={{
         enableRowSelection: true, // Enable row selection
         enableClickRowSelect: false, // Disable clicking rows to select them
